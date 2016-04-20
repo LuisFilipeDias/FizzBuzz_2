2f7edb696b3e0190a28eac03e26720196a3e2b65
Luis Filipe Dias <luis.dias@dtvresearch.com>
2016-04-20 14:11:17 +0100

    Implement step 2 and 3

--- main.cpp
+++ main.cpp
@@ -1,30 +1,55 @@
 #include <iostream>
-#include <string> 
+#include <string>
 
 using namespace std;
 
 #define START_INDEX 1
 #define END_INDEX 20
 
-void fizzBuzzer(int);
+string keywords[5] = {"fizz", "buzz" , "fizzbuzz", "lucky", "integer"};
+
+void fizzBuzzer(int, int *);
+void fizzBuzzerCountPrinter(int *);
 
 int main(int argc, char **argv)
 {
+    int counter[5] = {0, 0, 0, 0, 0};    
     for( int i = START_INDEX; i <= END_INDEX; i++)
-        fizzBuzzer(i);
+        fizzBuzzer(i, counter);
     cout << endl;
+    fizzBuzzerCountPrinter(counter);
 	return 0;
 }
 
-void fizzBuzzer(int number)
+void fizzBuzzerCountPrinter(int* counter)
 {
+    for( int i = 0; i < 5; i++)
+        cout << keywords[i] << ": " << counter[i] << endl;
+}
+
+void fizzBuzzer(int number, int* counter)
+{
+    int c_number = number;
+    
+    while(c_number != 0){
+        if(c_number % 10 == 3){
+            cout << keywords[3] << " ";
+            counter[3] ++;
+            return;
+        }
+        c_number = c_number / 10;
+    }    
     if (number % 15 == 0){
-        cout << "fizzbuzz ";
+        cout << keywords[2] << " ";
+        counter[2] ++;
     } else if (number % 5 == 0) {
-        cout << "buzz ";        
+        cout << keywords[1] << " ";
+        counter[1] ++;        
     } else if(number % 3 == 0) {
-        cout << "fizz ";
+        cout << keywords[0] << " ";
+        counter[0] ++;
     } else {
         cout << number << " ";
+        counter[4] ++;
     }
 }
